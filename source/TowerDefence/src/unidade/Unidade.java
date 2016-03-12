e6773c16cb406e35cf899fed7258151491556d2a
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2016-03-11 23:16:50 -0300

    Criação da classe Unidade

--- source/TowerDefence/src/unidade/Unidade.java
+++ source/TowerDefence/src/unidade/Unidade.java
@@ -1,5 +1,68 @@
 package unidade;
 
+import caminho.Caminho;
+import java.awt.Color;
+
 public class Unidade {
-    
+
+    protected Thread thread;
+    protected int x, y;
+    protected boolean vivo;
+    protected Color cor;
+    protected Caminho caminho;
+
+    public Unidade(int x, int y, Caminho caminho) {
+        this.x = x;
+        this.y = y;
+        this.caminho = caminho;
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    public Color getCor() {
+        return cor;
+    }
+
+    public void setCor(Color cor) {
+        this.cor = cor;
+    }
+
+    public Caminho getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(Caminho caminho) {
+        this.caminho = caminho;
+    }
+
 }
