bac919a938e6044e0ca941554f08fc1b9f2c490d
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2016-03-11 23:41:02 -0300

    Criação da base da classe Campo

--- source/TowerDefence/src/caminho/Campo.java
+++ source/TowerDefence/src/caminho/Campo.java
@@ -1,7 +1,18 @@
 package caminho;
 
 import javax.swing.JPanel;
+import selecao.Fase;
 
-public class Campo extends JPanel {
-
+public class Campo extends JPanel implements Runnable{
+    
+    private Thread thread;
+    private boolean jogo;
+    private int clock, moedas;
+    private Fase fase;
+    
+    @Override
+    public void run(){
+        
+    }
+    
 }
