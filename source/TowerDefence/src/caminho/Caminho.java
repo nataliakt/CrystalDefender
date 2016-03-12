71115c5731a1c587945afe872327afdf5fa45081
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2016-03-11 23:38:04 -0300

    Criação da base da classe Caminho

--- source/TowerDefence/src/caminho/Caminho.java
+++ source/TowerDefence/src/caminho/Caminho.java
@@ -1,5 +1,53 @@
 package caminho;
 
-public class Caminho {
+import java.util.ArrayList;
+import unidade.Inimigo;
+import unidade.Tiro;
+import unidade.Torre;
 
+public class Caminho {
+    
+    private ArrayList<Torre> torres;
+    private ArrayList<Tiro> tiros;
+    private ArrayList<Inimigo> inimigos;
+    int posicaoY;
+
+    public Caminho(int posicaoY) {
+        this.posicaoY = posicaoY;
+    }
+
+    public ArrayList<Torre> getTorres() {
+        return torres;
+    }
+
+    public void setTorres(ArrayList<Torre> torres) {
+        this.torres = torres;
+    }
+
+    public ArrayList<Tiro> getTiros() {
+        return tiros;
+    }
+
+    public void setTiros(ArrayList<Tiro> tiros) {
+        this.tiros = tiros;
+    }
+
+    public ArrayList<Inimigo> getInimigos() {
+        return inimigos;
+    }
+
+    public void setInimigos(ArrayList<Inimigo> inimigos) {
+        this.inimigos = inimigos;
+    }
+
+    public int getPosicaoY() {
+        return posicaoY;
+    }
+
+    public void setPosicaoY(int posicaoY) {
+        this.posicaoY = posicaoY;
+    }
+    
+    
+    
 }
